bf18ed866980c1a5051e3fe426219a474a6b1ac6
Ollivdh <olivier.vandenheede.mailbox@gmail.com>
2019-08-14 12:20:14 +0200

    Code snippet to set-up Firebase Authentication

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyCZDcXm1cVg7U46UyD7iidr4BL5qdHxzlY",
+    authDomain: "fir-authentication-6c84c.firebaseapp.com",
+    databaseURL: "https://fir-authentication-6c84c.firebaseio.com",
+    projectId: "fir-authentication-6c84c",
+    storageBucket: "",
+    messagingSenderId: "759275667057",
+    appId: "1:759275667057:web:80c50c88e73fb055"
+};
+
+export default config;
